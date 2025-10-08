41307efa17df2f0f402de9f84d616d67b0e0ce34
OmarF027 /c/wamp64/www/miei-progetti/calcolatrice (main)$ git config --global user.name OmarF027 <omarfrosali027@gmail.com>
2025-10-08 12:15:23 +0200

    Modifiche al progetto

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,37 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Ciao a tutti!",
+    user: "Mario",
+    added: new Date()
+  },
+  {
+    text: "Benvenuti nel message board!",
+    user: "Luigi", 
+    added: new Date()
+  }
+];
+
 indexRouter.get("/", (req, res) => {
-  res.render("index", { title: "Mini Messageboard" });
+  res.render("index", { 
+    title: "Mini Messageboard", 
+    messages: messages 
+  });
+});
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form", { title: "Nuovo Messaggio" });
+});
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  });
+  res.redirect("/");
 });
 
 module.exports = indexRouter;
